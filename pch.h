67d5d632d4928ae2b849eecefcaffd3787c22c7a
babaliaris <babaliaris.nikos@gmail.com>
2024-05-15 00:02:38 +0300

    Init

---
+++ pch.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include "spdlog/sinks/basic_file_sink.h"
---
+++ premake5.lua
@@ -0,0 +1,69 @@
+workspace "TestSPDlog"
+    configurations {"debug", "dist", "release"}
+    platforms "x64"
+
+    include "../spdlog"
+
+    project "TestSPDLOG"
+        pchheader "pch.h"
+        pchsource "pch.cpp"
+        targetname "testspdlog"
+        kind "ConsoleApp"
+        language "C++"
+        cppdialect "c++17"
+        targetdir "%{_WORKING_DIR}/builds/%{cfg.shortname}/"
+        objdir "%{_WORKING_DIR}/obj/%{prj.name}_%{cfg.shortname}/"
+
+        files {
+            "./**.h",
+            "./**.hpp",
+            "./**.cpp",
+            "./**.c"
+        }
+
+        includedirs {
+            ".",
+            "../spdlog/include"
+
+        }
+
+        links {
+            "spdlog"
+        }
+
+        defines {
+            "SPDLOG_COMPILED_LIB"
+        }
+
+        -- Debug All Operate Systems & Configurations
+        filter{}
+        filter "configurations:debug"
+            runtime "Debug"
+            symbols "on"
+
+        -- Dist All Operate Systems & Configurations
+        filter{}
+        filter "configurations:dist"
+            runtime "Release"
+            optimize "on"
+
+        -- Release All Operate Systems & Configurations
+        filter{}
+        filter "configurations:release"
+            runtime "Release"
+            optimize "on"
+
+        -- Linux All Configurations
+        filter{}
+        filter "system:Linux"
+            defines {
+                "VAMP_LINUX"
+            }
+
+
+        -- Windows All Configurations
+        filter{}
+        filter "system:Windows"
+            defines {
+                "VAMP_WINDOWS"
+            }
